11f85e18731e185eea95c0c2240d2b80c7258f32
Rudraksh Gupta <udashiv18@gmail.com>
2025-12-07 20:25:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const Score = JSON.parse(localStorage.getItem('Score')) ||{
+    Wins: 0,
+    Losses: 0,
+    Ties: 0
+};
+function Reset(){
+    Score.Losses = 0;
+    Score.Ties = 0;
+    Score.Wins = 0;
+    localStorage.removeItem('Score')
+}
+function Playgame(Playermove){
+    const Computermove = Pickcomputermove()
+    let result = '';
+    if (Playermove === 'Scissor'){
+        if (Computermove === 'Rock'){
+                result = 'You Lost';
+        }else if (Computermove === 'Paper'){
+            result = 'You Win';
+        }else if(Computermove === 'Scissor'){
+            result = 'Tie';
+        }    
+    }
+    else if (Playermove === 'Paper'){
+        if (Computermove === 'Rock'){
+        result = 'You Win';
+        }else if (Computermove === 'Paper'){
+            result = 'Tie';
+        }else if(Computermove === 'Scissor'){
+            result = 'You Lost';
+        }
+
+    }
+    else if (Playermove === 'Rock'){
+        if (Computermove === 'Rock'){
+        result = 'Tie';
+        }else if (Computermove === 'Paper'){
+            result = 'You Lost';
+        }else if(Computermove === 'Scissor'){
+            result = 'You Win';
+        }
+    }
+    if (result === 'You Win'){
+        Score.Wins +=1;
+    }else if(result === 'You Lost'){
+        Score.Losses +=1
+    }else if(result === 'Tie'){
+        Score.Ties +=1
+    }
+    localStorage.setItem('Score', JSON.stringify(Score));
+    alert(`You picked ${Playermove}. Computer picked ${Computermove}. ${result}\n Wins: ${Score.Wins} Loss: ${Score.Losses} Tie: ${Score.Ties}`);
+}
+function Pickcomputermove(){
+    const randomnum = Math.random();
+    let Computermove = '';
+    if (randomnum >=0 && randomnum < 1/3){
+        Computermove = 'Rock';
+    }else if (randomnum >=1/3 && randomnum < 2/3){
+        Computermove = 'Paper';
+    }else{
+        Computermove = 'Scissor';
+    }
+    return Computermove;
+}
